0acec8a5c4474559dafbb5bcb4ea248f612c9171
Esdras Pavon <pavonesdras@gmail.com>
2019-12-02 23:39:36 -0300

    changes in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path') // Modulo de node y encargado de la gestion de rutas
 
 module.exports = {
-    entry: './index.js', // Ruta del archivo principal de nuestra aplicación JS a ser procesado por Webpack. Se pueden tener varios Entry Points.
+    entry: path.resolve(__dirname, '/index.js'), // Ruta del archivo principal de nuestra aplicación JS a ser procesado por Webpack. Se pueden tener varios Entry Points.
     mode: 'development', // Modo de ejecucion
     output:{ // Configuraciones para el archivo final que generará webpack
-        path: path.resolve(__dirname), // Direccion relativa a la posicion actual donde generará el bundle
+        path: path.resolve(__dirname, 'dist'), // Direccion relativa a la posicion actual donde generará el bundle
         filename: 'bundle.js' // Nombre final del bundle
     }
 }
