442bf8121fff73a0554568899548674a1eba4e4d
Esdras Pavon <pavonesdras@gmail.com>
2019-12-03 11:05:13 -0300

    multi entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
-const path = require('path') // Modulo de node y encargado de la gestion de rutas
+const path = require("path"); // Modulo de node y encargado de la gestion de rutas
 
 module.exports = {
-    entry: path.resolve(__dirname, '/index.js'), // Ruta del archivo principal de nuestra aplicación JS a ser procesado por Webpack. Se pueden tener varios Entry Points.
-    mode: 'development', // Modo de ejecucion
-    output:{ // Configuraciones para el archivo final que generará webpack
-        path: path.resolve(__dirname, 'dist'), // Direccion relativa a la posicion actual donde generará el bundle
-        filename: 'bundle.js' // Nombre final del bundle
-    }
-}
+  entry: {
+    home: path.resolve(__dirname, "src/js/index.js"),
+    precios: path.resolve(__dirname, "src/js/precios.js"),
+    contacto: path.resolve(__dirname, "src/js/contacto.js")
+  }, // Ruta del archivo principal de nuestra aplicación JS a ser procesado por Webpack. Se pueden tener varios Entry Points.
+  mode: "development", // Modo de ejecucion
+  output: {
+    // Configuraciones para el archivo final que generará webpack
+    path: path.resolve(__dirname, "dist"), // Direccion relativa a la posicion actual donde generará el bundle
+    filename: "js/[name].js" // Nombre final del bundle, [name] corresponde al key de cada entry
+  }
+};
