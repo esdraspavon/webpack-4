ca3afb77fb34e557750eff6a1e69d52bdef2034e
Esdras Pavon <pavonesdras@gmail.com>
2019-12-03 15:32:55 -0300

    installing babel

---
+++ babel/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path"); // Modulo de node y encargado de la gestion de rutas
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin"); // Modulo para empaquetar css fuera del js
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //Modulo para generar un html con la configuracion de webpack, js, css, etc
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "src/js/index.js")
+  },
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  devServer: {
+    hot: true, //Activamos el hot module replacement
+    open: true, // Abrir una pestaña del navegador al correr el sv
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          //Se comenta en desarrollo, porque es menos costoso añadir los archivos directo al archivo, que crear un nuevo archivo
+          // {
+          //   loader: MiniCSSExtractPlugin.loader // separa los archivos css
+          // },
+          "style-loader", // Inyecta el css al html
+          "css-loader" //Carge los archivos css en js
+        ]
+      },
+      {
+        test: /\.js$/, //archivos js
+        use: "babel-loader", // traspila codigo js
+        exclude: /node_modules/ //omitir traspilar codigo que se encuentra aqui
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), //Plugin para que solo recarge los elementos modificados en el html y no toda la pagina
+    new HtmlWebpackPlugin({
+      title: "webpack-dev-server" // Titulo del documento
+    })
+    //Se comenta en desarrollo, porque es menos costoso añadir los estilos directo al archivo, que crear un nuevo archivo
+    // new MiniCSSExtractPlugin({
+    //   filename: "css/[name].css" // direccion y/o nombre del archivo final
+    // })
+  ]
+};
